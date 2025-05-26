c90458037d594dfc9d7c23bf589e241a43d34f18
nairobi-kl <kulibchuk.a2006@gmail.com>
2025-05-26 04:11:01 +0300

    Add Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
-import MovieList from '../components/MovieList';
-import { moviesData } from '../data/movies';
+import { Link } from 'react-router-dom';
+import logo from '../logo.jpeg'; 
 
-const Home = () => <MovieList movies={moviesData} />;
+const Home = ({ movies }) => {
+  return (
+    <div className="home-page">
+      <div className="container">
+        <h1 className="page-title">Ласкаво просимо до першого дівочого кінотеатру!</h1>
+        <p className="page-subtitle">Переглядайте фільми, що доступні у прокаті та бронюйте квитки</p>
+        <img src={logo} alt="PinkCinema Logo" className="home-logo" />
+      </div>
+    </div>
+  );
+};
+
 export default Home;
-
