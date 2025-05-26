0844831963f299d365b6d2e550c58e6891461a11
nairobi-kl <kulibchuk.a2006@gmail.com>
2025-05-26 04:13:23 +0300

    Add Booking page with CinemaHall and movie id from useParams

--- src/pages/Booking.jsx
+++ src/pages/Booking.jsx
@@ -1,4 +1,18 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import CinemaHall from '../components/CinemaHall';
 
-const Booking = () => <div>Booking Page</div>;
+const Booking = () => {
+  const { id } = useParams();
+
+  return (
+    <div className="booking-page">
+      <div className="container">
+        <h1>Бронювання фільму ID: {id}</h1>
+        <CinemaHall />
+      </div>
+    </div>
+  );
+};
+
 export default Booking;
